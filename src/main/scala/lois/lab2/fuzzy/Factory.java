d657fa8e88731446e38266fd27c36f732b0a5c2d
Yasko Alexei <alexei.yasko@gmail.com>
2012-12-04 13:03:35 +0300

    small fixes

--- src/main/scala/lois/lab2/fuzzy/Factory.java
+++ src/main/scala/lois/lab2/fuzzy/Factory.java
@@ -1,15 +1,16 @@
 package lois.lab2.fuzzy;
 
-import java.util.List;
+//import java.util.List;
 
 import scala.Tuple2;
+import scala.collection.immutable.List;
 
 /**
  * @author Q-YAA
  */
 public class Factory {
 
-    public static FuzzySet createFuzzySet(String name, List<Tuple2> elements) {
+    public static FuzzySet createFuzzySet(String name, List<Tuple2<String, scala.Float>> elements) {
         return new FuzzySet(name, elements);
     }
 }
